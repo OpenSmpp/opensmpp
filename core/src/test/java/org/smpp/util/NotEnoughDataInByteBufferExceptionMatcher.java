e126f18c6fff9376a6b404644fa2a2e94f2b3f0b
ptomli <ptomli@gmail.com>
2013-02-11 23:19:03 +0200

    some tests for ByteBuffer

---
+++ core/src/test/java/org/smpp/util/NotEnoughDataInByteBufferExceptionMatcher.java
@@ -0,0 +1,33 @@
+package org.smpp.util;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+public class NotEnoughDataInByteBufferExceptionMatcher extends TypeSafeMatcher<NotEnoughDataInByteBufferException>{
+
+	private final int expected;
+	private final int available;
+
+	public static NotEnoughDataInByteBufferExceptionMatcher notEnoughData(int expected, int available) {
+		return new NotEnoughDataInByteBufferExceptionMatcher(expected, available);
+	}
+
+	public NotEnoughDataInByteBufferExceptionMatcher(int expected, int available) {
+		this.expected = expected;
+		this.available = available;
+	}
+
+	@Override
+	public void describeTo(Description description) {
+		description
+			.appendText("expected ")
+			.appendValue(expected)
+			.appendText("available ")
+			.appendValue(available);
+	}
+
+	@Override
+	protected boolean matchesSafely(NotEnoughDataInByteBufferException ex) {
+		return ex.getExpected() == expected && ex.getAvailable() == available;
+	}
+}
