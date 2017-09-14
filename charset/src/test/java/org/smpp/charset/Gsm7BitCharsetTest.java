e87c60a998553b2688a6d4757679507b897b6e70
Paul Tomlin <ptomli@gmail.com>
2017-09-14 10:36:06 +0200

    Add tests for OpenSmpp/opensmpp#16

---
+++ charset/src/test/java/org/smpp/charset/Gsm7BitCharsetTest.java
@@ -0,0 +1,34 @@
+package org.smpp.charset;
+
+import static org.junit.Assert.assertEquals;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CoderResult;
+import java.nio.charset.spi.CharsetProvider;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Gsm7BitCharsetTest {
+
+	private Charset charset; 
+
+	@Before
+	public void setup() {
+		CharsetProvider provider = new Gsm7BitCharsetProvider();
+		
+		charset = provider.charsetForName("X-Gsm7Bit");
+	}
+
+	@Test
+	public void testEncoderOverflow() {
+		assertEquals(CoderResult.OVERFLOW, charset.newEncoder().encode(CharBuffer.wrap("00"), ByteBuffer.wrap(new byte[1]), true));
+	}
+
+	@Test
+	public void testDecoderOverflow() {
+		assertEquals(CoderResult.OVERFLOW, charset.newDecoder().decode(ByteBuffer.wrap(new byte[] { 0x30, 0x30 }), CharBuffer.allocate(1), true));
+	}
+}
