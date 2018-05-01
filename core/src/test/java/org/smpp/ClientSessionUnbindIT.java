0e84fb32357c0725f583c101098fb77137a01e07
Paolo Campanella <paolo@thefungus.net>
2018-05-01 21:39:56 +0200

    Add integration tests for slow session shutdown #33
    * Added failsafe plugin for integration tests.
    * Added embedded SMPP TestServer for tests to run against.
    * Added tests to demonstrate the effects of certain config settings on
      shutdown times.

---
+++ core/src/test/java/org/smpp/ClientSessionUnbindIT.java
@@ -0,0 +1,134 @@
+package org.smpp;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.smpp.TestServer.PduResponder;
+import org.smpp.pdu.BindRequest;
+import org.smpp.pdu.BindResponse;
+import org.smpp.pdu.BindTransmitter;
+import org.smpp.pdu.Response;
+import org.smpp.pdu.Unbind;
+import org.smpp.pdu.UnbindResp;
+
+public class ClientSessionUnbindIT extends SmppObject {
+	int listenPort = 0;
+	PduResponder responder;
+	TestServer testServer;
+	Session clientSession;
+
+    
+	@Before
+	public void setup() {
+		// Uncomment to enable:
+		// TestUtils.eventsToStdout();
+		// TestUtils.debuggingToStdout();
+		
+		responder = Mockito.mock(PduResponder.class);
+		
+		testServer = new TestServer(0, responder); // 0: run server on ephemeral port
+
+		Thread serverThread = new Thread(testServer, "server");
+		serverThread.setDaemon(true);
+		serverThread.start();
+		listenPort = testServer.getPort(); // Find ephemeral port allocated
+	}
+	
+	@After
+	public void tearDown() throws InterruptedException {
+		// Shut the server down if a test hasn't already chosen to do so itself:
+		if (!testServer.isShutDown()) { testServer.stop(); }
+		while (!testServer.isShutDown()) { Thread.sleep(100); }
+	}
+	
+	/**
+	 * See https://github.com/OpenSmpp/opensmpp/issues/33
+	 */
+	@Test(timeout=5000)
+	public void unbindSucceedsWithinFiveSeconds() throws Exception {
+		// Too high a commsTimeout on the clientConnection will cause this test to time out:
+		bind();
+		when(responder.getResponse(any(Unbind.class))).thenAnswer(mockLoggingUnbindResponse());
+		unbind();
+	}
+	
+	@Test(timeout=8000)
+	public void serverShutsDownWithinEightSeconds() throws Exception {
+		bind();
+		when(responder.getResponse(any(Unbind.class))).thenAnswer(mockLoggingUnbindResponse());
+		unbind();
+		// This should make the test time out if e.g. the receiverQueueWaitTimeout is too long in TestServer
+		// (and there has been certain async activity on the link, such at the unbind above):
+		testServer.stop();
+		while (!testServer.isShutDown()) { Thread.sleep(100); }
+	}
+	
+	private Answer<Response> mockLoggingUnbindResponse( ) {
+		return new Answer<Response>() {
+			public Response answer(InvocationOnMock invocation) throws Throwable {
+				return ((Unbind) invocation.getArguments()[0]).getResponse();
+			}
+		};
+	}
+
+	private void unbind() throws Exception {
+		clientDebug("Sending unbind request");
+		UnbindResp unbindResp = clientSession.unbind();
+		assertFalse(clientSession.isBound());
+		assertNotNull(unbindResp);
+		assertTrue(unbindResp.isOk());
+		clientDebug("Unbind: success");
+	}
+	
+	private void bind() throws Exception {
+		clientEvent("connecting");
+		TCPIPConnection clientConnection = new TCPIPConnection("127.0.0.1", listenPort);
+		clientConnection.setReceiveTimeout(2 * 1000);
+		clientConnection.setCommsTimeout(1 * 1000);
+
+		clientSession = new Session(clientConnection);
+		clientSession.open();
+		
+		BindRequest bindReq = new BindTransmitter();
+		bindReq.assignSequenceNumber();
+		bindReq.setSystemId("asd");
+		bindReq.setPassword("asd");
+		bindReq.setSystemType("asd");
+		bindReq.setInterfaceVersion((byte) 0x34);
+		bindReq.setAddressRange("1*");
+
+		Response bindResp = ((BindRequest) bindReq).getResponse();
+		bindResp.setCommandStatus(Data.ESME_ROK);
+		when(responder.getResponse(any(BindRequest.class))).thenReturn(bindResp);
+
+		clientEvent("Sending bind request");
+		ServerPDUEventListener listener = new ServerPDUEventListener(){
+			public void handleEvent(ServerPDUEvent event) {
+				debug.write("Event: " + event.getPDU().debugString());
+			}};
+		BindResponse response = clientSession.bind(bindReq, listener);
+		assertNotNull(response);
+		clientEvent("Bind response " + response.debugString());
+		assertEquals(Data.ESME_ROK, response.getCommandStatus());
+		assertEquals(bindResp.getSequenceNumber(), response.getSequenceNumber());
+	}
+
+	
+	private void clientEvent (String s) {
+		event.write("[Client]: " + s);
+	}
+	private void clientDebug (String s) {
+		debug.write("[Client]: " + s);
+	}
+
+}
---
+++ core/src/test/java/org/smpp/TestServer.java
@@ -0,0 +1,192 @@
+package org.smpp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.smpp.pdu.PDU;
+import org.smpp.pdu.Response;
+
+/**
+ * An embedded SMPP server for tests to run against.
+ *
+ */
+class TestServer extends SmppObject implements Runnable {
+	private PduResponder responder;
+	private volatile boolean keepRunning = true;
+	private volatile boolean shutDown = false;
+	private volatile boolean running = false;
+	private final int listenPort;
+	private TCPIPConnection serverConn = null;
+
+	// The defaults in Data are all around 60 seconds - set lower to speed up test completion:
+	private static final int GLOBAL_DEFAULT_TIMEOUT = 4 * 1000;
+	private long connectionReceiveTimeout = GLOBAL_DEFAULT_TIMEOUT;
+	private int connectionConnectTimeout = GLOBAL_DEFAULT_TIMEOUT;
+	private long connectionSocketCommsTimeout = GLOBAL_DEFAULT_TIMEOUT;
+	private long receiverReceiveTimeout = GLOBAL_DEFAULT_TIMEOUT;
+	private long acceptedSocketCommsTimeout = GLOBAL_DEFAULT_TIMEOUT;
+	private long acceptedSocketReceiveTimeout = GLOBAL_DEFAULT_TIMEOUT;
+	// This setting is especially detrimental to quick server shutdowns, and seems harmless to
+	// decrease (should have zero negative effect):
+	private long receiverQueueWaitTimeout = 1 * 1000;
+	
+	public void stop() {
+		this.keepRunning = false;
+	}
+	public boolean isShutDown() {
+		return this.shutDown;
+	}
+	public boolean isRunning() {
+		return running;
+	}
+	/**
+	 * 
+	 * @param listenPort a port to listen on, or 0 to use an ephemeral port
+	 * @param responder
+	 */
+	public TestServer(int listenPort, PduResponder responder) {
+		this.listenPort = listenPort;
+		this.responder = responder;
+	}
+	
+	public void run() {
+		try {
+			serverEvent("Trying to listen on port: " + ((listenPort > 0) ? String.valueOf(listenPort) : "<ephemeral>"));
+			serverConn = new org.smpp.TCPIPConnection(listenPort);
+			serverConn.setCommsTimeout(getConnectionSocketCommsTimeout());
+			serverConn.setConnectionTimeout(getConnectionConnectTimeout());
+			serverConn.setReceiveTimeout(getConnectionReceiveTimeout());
+			serverConn.open();
+			serverEvent("Listening on port: " + serverConn.getPort());
+			running = true;
+
+			Connection connection = serverConn.accept();
+			connection.setCommsTimeout(acceptedSocketCommsTimeout);
+			connection.setReceiveTimeout(acceptedSocketReceiveTimeout);
+			serverEvent("Connection accepted");
+
+			Transmitter transmitter = new Transmitter(connection);
+			Receiver receiver = new ServerReceiver(transmitter, connection);
+
+			serverEvent("Starting receiver");
+			receiver.setReceiveTimeout(getReceiverReceiveTimeout());
+			receiver.setQueueWaitTimeout(getReceiverQueueWaitTimeout());
+			receiver.start();
+			
+			serverDebug("Trying to receive PDUs...");
+			while (keepRunning) {
+				// While this may look like it won't keep things hung up for long, it is in fact entirely
+				// dependent on receiver.queueWaitTimeout in certain cases (i.e. if a PDU isn't immediately
+				// available in the pduQueue - see Receiver.tryReceivePDU()):
+				PDU pdu = receiver.receive(500);
+				if (pdu == null) { continue; }
+				serverDebug("PDU received: " + pdu.debugString());
+				assertTrue(pdu.isOk());
+				assertTrue(pdu.isRequest());
+
+				PDU responsePdu = responder.getResponse(pdu);
+				
+				serverDebug("Sending response: " + responsePdu.debugString());
+				transmitter.send(responsePdu);
+				serverDebug("Response sent: " + responsePdu);
+			}
+			serverEvent("shutting down");
+			receiver.stop();
+			serverConn.close();
+			serverEvent("shut down");
+		} catch (Exception e) {
+			serverEvent("Exception shutting down: " + e);
+			e.printStackTrace();
+		} finally {
+			this.shutDown = true;
+			running = false;
+		}
+	}
+	
+	private long getConnectionReceiveTimeout() {
+		return connectionReceiveTimeout;
+	}
+	public void setConnectionReceiveTimeout(long connectionReceiveTimeout) {
+		this.connectionReceiveTimeout = connectionReceiveTimeout;
+	}
+
+	private int getConnectionConnectTimeout() {
+		return connectionConnectTimeout;
+	}
+	public void setConnectionConnectTimeout(int connectionConnectTimeout) {
+		this.connectionConnectTimeout = connectionConnectTimeout;
+	}
+
+	private long getConnectionSocketCommsTimeout() {
+		return connectionSocketCommsTimeout;
+	}
+	public void setConnectionSocketCommsTimeout(long connectionSocketCommsTimeout) {
+		this.connectionSocketCommsTimeout = connectionSocketCommsTimeout;
+	}
+
+	private long getReceiverQueueWaitTimeout() {
+		return receiverQueueWaitTimeout;
+	}
+	public void setReceiverQueueWaitTimeout(long receiverQueueWaitTimeout) {
+		this.receiverQueueWaitTimeout = receiverQueueWaitTimeout;
+	}
+
+	private long getReceiverReceiveTimeout() {
+		return receiverReceiveTimeout;
+	}
+	public void setReceiverReceiveTimeout(long receiverReceiveTimeout) {
+		this.receiverReceiveTimeout = receiverReceiveTimeout;
+	}
+	
+	public long getAcceptedSocketCommsTimeout() {
+		return acceptedSocketCommsTimeout;
+	}
+	public void setAcceptedSocketCommsTimeout(long acceptedSocketCommsTimeout) {
+		this.acceptedSocketCommsTimeout = acceptedSocketCommsTimeout;
+	}
+	
+	public long getAcceptedSocketReceiveTimeout() {
+		return acceptedSocketReceiveTimeout;
+	}
+	public void setAcceptedSocketReceiveTimeout(long acceptedSocketReceiveTimeout) {
+		this.acceptedSocketReceiveTimeout = acceptedSocketReceiveTimeout;
+	}
+
+	public int getPort() {
+		int iterations = 0;
+		int sleepTime = 100;
+		while (!running) {
+			try {
+				Thread.sleep(sleepTime);
+			} catch (InterruptedException e) { }
+			if (sleepTime * iterations >  5 * 1000) {
+				throw new RuntimeException("Could not get port - server not found running within 5 seconds");
+			}
+		}
+		return serverConn.getPort();
+	}
+
+	class ServerReceiver extends Receiver {
+		public ServerReceiver(Connection connection) {
+			super(connection);
+		}
+		public ServerReceiver(Transmitter transmitter, Connection connection) {
+			super(transmitter, connection);
+		}
+		@Override
+		public String getThreadName() {
+			return "ServerReceiver";
+		}		
+	}
+
+	interface PduResponder {
+		public Response getResponse(PDU pdu);
+	}
+
+	void serverDebug (String s) {
+		debug.write("[Server]: " + s);
+	}
+	void serverEvent (String s) {
+		event.write("[Server]: " + s);
+	}	
+
+}
